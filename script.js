0657a66bbb49537a29f68084c2fed6b3f545aee6
igor <igorkalutskiy@outlook.com>
2020-01-20 02:01:41 +0300

    firt commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//'use strict';
+
+a = 5;
+console.log(a)
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol;
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9)
+
+let something;
+console.log(something)
+
+let persone ={ 
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+//alert('Hello world');
+
+//let answer = +prompt("Are you here?");
+
+//console.log(typeo(answer));
+
+//let answer = +prompt ("Eсть ли Вам 18","Да");
+
+//
+
+let incr = 10,
+   decr=10; //всегда заканчивать ; иначе будет идти как новая переменная 
+
+
+
+console.log(incr++)
+console.log(decr--)
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
